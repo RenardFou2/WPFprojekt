5aa9943f5a34cf0bd1410a9053551f199ae03e74
Renard <harpi473@Gmail.com>
2024-05-31 22:54:14 +0200

    Drukowanie

--- WPFprojekt/WeeklyScheduleWindow.xaml.cs
+++ WPFprojekt/WeeklyScheduleWindow.xaml.cs
@@ -20,11 +20,57 @@ namespace WPFprojekt
         public WeeklyScheduleWindow(ObservableCollection<Seans> seanse, DateTime startOfWeek)
         {
             InitializeComponent();
+            DataContext = this;
 
             var endOfWeek = startOfWeek.AddDays(7);
             var weeklySeanse = seanse.Where(s => s.Czas >= startOfWeek && s.Czas < endOfWeek).ToList();
 
-            WeeklySeansListBox.ItemsSource = weeklySeanse;
+            WeeklyScheduleListView.ItemsSource = weeklySeanse;
+        }
+        private void PrintSchedule_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                FlowDocument doc = new FlowDocument();
+                doc.PagePadding = new Thickness(50);
+                doc.PageWidth = 800;
+
+                Table table = new Table();
+                doc.Blocks.Add(table);
+
+                for (int i = 0; i < 6; i++)
+                {
+                    table.Columns.Add(new TableColumn());
+                }
+
+                TableRow headerRow = new TableRow();
+                table.RowGroups.Add(new TableRowGroup());
+                table.RowGroups[0].Rows.Add(headerRow);
+
+                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Tytuł"))));
+                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Data"))));
+                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Sala"))));
+                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Cena biletu"))));
+                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Czas"))));
+                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Gatunek"))));
+
+                foreach (Seans seans in WeeklyScheduleListView.Items)
+                {
+                    TableRow row = new TableRow();
+                    table.RowGroups[0].Rows.Add(row);
+
+                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Nazwa))));
+                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Czas.ToString()))));
+                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Sala))));
+                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Cena_biletu.ToString("C")))));
+                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Dlugosc.ToString()))));
+                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Gatunek))));
+                }
+
+                IDocumentPaginatorSource idpSource = doc;
+                printDialog.PrintDocument(idpSource.DocumentPaginator, "Harmonogram");
+            }
         }
     }
 }
