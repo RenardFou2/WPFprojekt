c3ecf787cc2d249bf0cf1cb64a98bf40713f55d0
Syadaisu <daszymanski001@gmail.com>
2024-06-18 18:44:52 +0200

    diagram filmow w tygodniu

---
+++ WPFprojekt/StartPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace WPFprojekt
+{
+	public partial class StartPage : Window
+	{
+		public StartPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			Storyboard storyboard = (Storyboard)this.Resources["TextAnimation"];
+
+			storyboard.Begin();
+		}
+
+		private void btnStart_Click(object sender, RoutedEventArgs e)
+		{
+			App.Current.MainWindow = new MainWindow();
+
+			App.Current.MainWindow.Show();
+
+			this.Close();
+		}
+
+
+	}
+}
--- WPFprojekt/WeeklyScheduleWindow.xaml.cs
+++ WPFprojekt/WeeklyScheduleWindow.xaml.cs
@@ -2,75 +2,181 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace WPFprojekt
 {
-    public partial class WeeklyScheduleWindow : Window
-    {
-        public WeeklyScheduleWindow(ObservableCollection<Seans> seanse, DateTime startOfWeek)
-        {
-            InitializeComponent();
-            DataContext = this;
+	public partial class WeeklyScheduleWindow : Window
+	{
+		private ObservableCollection<Seans> seanse;
+		private DateTime startOfWeek;
+		private Dictionary<DateTime, int> dailyCount;
+		private bool isLoaded = false;
 
-            var endOfWeek = startOfWeek.AddDays(7);
-            var weeklySeanse = seanse.Where(s => s.Czas >= startOfWeek && s.Czas < endOfWeek).ToList();
+		public WeeklyScheduleWindow(ObservableCollection<Seans> seanse, DateTime startOfWeek)
+		{
+			InitializeComponent();
+			DataContext = this;
 
-            WeeklyScheduleListView.ItemsSource = weeklySeanse;
-        }
-        private void PrintSchedule_Click(object sender, RoutedEventArgs e)
-        {
-            PrintDialog printDialog = new PrintDialog();
-            if (printDialog.ShowDialog() == true)
-            {
-                FlowDocument doc = new FlowDocument();
-                doc.PagePadding = new Thickness(50);
-                doc.PageWidth = 800;
+			this.seanse = seanse;
+			this.startOfWeek = startOfWeek;
 
-                Table table = new Table();
-                doc.Blocks.Add(table);
+			var endOfWeek = startOfWeek.AddDays(7);
+			var weeklySeanse = seanse.Where(s => s.Czas >= startOfWeek && s.Czas < endOfWeek).ToList();
 
-                for (int i = 0; i < 6; i++)
-                {
-                    table.Columns.Add(new TableColumn());
-                }
+			WeeklyScheduleListView.ItemsSource = weeklySeanse;
 
-                TableRow headerRow = new TableRow();
-                table.RowGroups.Add(new TableRowGroup());
-                table.RowGroups[0].Rows.Add(headerRow);
+			PopulateDailyCount();
+			PopulateSeansTable();
 
-                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Tytuł"))));
-                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Data"))));
-                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Sala"))));
-                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Cena biletu"))));
-                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Czas"))));
-                headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Gatunek"))));
+			// Subskrypcja zdarzenia Loaded okna
+			Loaded += WeeklyScheduleWindow_Loaded;
+		}
 
-                foreach (Seans seans in WeeklyScheduleListView.Items)
-                {
-                    TableRow row = new TableRow();
-                    table.RowGroups[0].Rows.Add(row);
+		private void WeeklyScheduleWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			isLoaded = true;
+			DrawChart();
+		}
 
-                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Nazwa))));
-                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Czas.ToString()))));
-                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Sala))));
-                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Cena_biletu.ToString("C")))));
-                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Dlugosc.ToString()))));
-                    row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Gatunek))));
-                }
+		private void PopulateDailyCount()
+		{
+			dailyCount = new Dictionary<DateTime, int>();
 
-                IDocumentPaginatorSource idpSource = doc;
-                printDialog.PrintDocument(idpSource.DocumentPaginator, "Harmonogram");
-            }
-        }
-    }
+			for (DateTime date = startOfWeek; date < startOfWeek.AddDays(7); date = date.AddDays(1))
+			{
+				dailyCount[date.Date] = 0;
+			}
+
+			foreach (Seans seans in seanse)
+			{
+				DateTime dateKey = seans.Czas.Date;
+				if (dailyCount.ContainsKey(dateKey))
+				{
+					dailyCount[dateKey]++;
+				}
+			}
+		}
+
+		private void DrawChart()
+		{
+			if (!isLoaded || dailyCount == null || chartCanvas.ActualWidth == 0 || chartCanvas.ActualHeight == 0)
+				return;
+
+			double maxCount = dailyCount.Values.Max();
+			double barWidth = chartCanvas.ActualWidth / dailyCount.Count;
+			double maxBarHeight = chartCanvas.ActualHeight;
+
+			chartCanvas.Children.Clear();
+
+			int index = 0;
+			foreach (var kvp in dailyCount)
+			{
+				double barHeight = (kvp.Value / maxCount) * maxBarHeight;
+
+				Rectangle rect = new Rectangle();
+				rect.Width = barWidth;
+				rect.Height = barHeight;
+				rect.Fill = Brushes.DarkBlue;
+
+				Canvas.SetLeft(rect, index * barWidth + 10);
+				Canvas.SetTop(rect, maxBarHeight - barHeight);
+
+				chartCanvas.Children.Add(rect);
+
+				TextBlock textBlock = new TextBlock();
+				textBlock.Text = kvp.Value.ToString();
+				textBlock.TextAlignment = TextAlignment.Center;
+				textBlock.FontSize = 12;
+
+
+				Canvas.SetLeft(textBlock, index * barWidth + barWidth / 2);
+				Canvas.SetTop(textBlock, maxBarHeight - barHeight - 20);
+
+				chartCanvas.Children.Add(textBlock);
+
+
+				TextBlock dateTextBlock = new TextBlock();
+				dateTextBlock.Text = kvp.Key.ToShortDateString();
+				dateTextBlock.TextAlignment = TextAlignment.Center;
+				dateTextBlock.FontSize = 12;
+
+
+				Canvas.SetLeft(dateTextBlock, index * barWidth + 10);
+				Canvas.SetTop(dateTextBlock, maxBarHeight + 5);
+
+				chartCanvas.Children.Add(dateTextBlock);
+
+				index++;
+			}
+		}
+
+		private void PopulateSeansTable()
+		{
+			if (WeeklyScheduleListView.Items == null)
+				return;
+
+			var seansList = WeeklyScheduleListView.Items.Cast<Seans>().Select(seans => new
+			{
+				seans.film.Nazwa,
+				seans.Czas,
+				seans.Sala,
+				Cena_biletu = seans.Cena_biletu.ToString("C"),
+				seans.film.Dlugosc,
+				seans.film.Gatunek
+			}).ToList();
+
+		}
+
+		private void PrintSchedule_Click(object sender, RoutedEventArgs e)
+		{
+			PrintDialog printDialog = new PrintDialog();
+			if (printDialog.ShowDialog() == true)
+			{
+				FlowDocument doc = new FlowDocument();
+				doc.PagePadding = new Thickness(50);
+				doc.PageWidth = 800;
+
+				Table table = new Table();
+				doc.Blocks.Add(table);
+
+				for (int i = 0; i < 6; i++)
+				{
+					table.Columns.Add(new TableColumn());
+				}
+
+				TableRowGroup group = new TableRowGroup();
+				table.RowGroups.Add(group);
+
+				TableRow headerRow = new TableRow();
+				group.Rows.Add(headerRow);
+				headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Tytuł"))));
+				headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Data"))));
+				headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Sala"))));
+				headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Cena biletu"))));
+				headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Długość seansu"))));
+				headerRow.Cells.Add(new TableCell(new Paragraph(new Run("Gatunek"))));
+
+				foreach (Seans seans in seanse)
+				{
+					TableRow row = new TableRow();
+					group.Rows.Add(row);
+
+					row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Nazwa))));
+					row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Czas.ToString()))));
+					row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Sala.ToString()))));
+					row.Cells.Add(new TableCell(new Paragraph(new Run(seans.Cena_biletu.ToString("C")))));
+					row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Dlugosc.ToString()))));
+					row.Cells.Add(new TableCell(new Paragraph(new Run(seans.film.Gatunek))));
+				}
+
+				IDocumentPaginatorSource idocument = doc as IDocumentPaginatorSource;
+				printDialog.PrintDocument(idocument.DocumentPaginator, "Harmonogram seansów");
+			}
+		}
+	}
 }
